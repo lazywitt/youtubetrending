8f7de94a2677d5524d5d3ed3d6cf709f65fb6942
lazywitt <tushargupta81@gmail.com>
2023-01-13 15:38:32 +0530

    initialise an http server to serve api requests

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"gorm.io/driver/postgres"
@@ -11,6 +12,7 @@ import (
 
 	"github.com/lazywitt/youtubetrending/db"
 	"github.com/lazywitt/youtubetrending/db/models"
+	"github.com/lazywitt/youtubetrending/fetch"
 	"github.com/lazywitt/youtubetrending/scraper"
 )
 
@@ -30,10 +32,25 @@ func main() {
 		fmt.Printf("error Initialising DB and entities: %v\n", err)
 	}
 
+	log.Println("start scraping")
 	youtubeService := scraper.GetYoutubeSeed(scraperConf.SearchResource, scraperConf.SearchKey, scraperConf.OrderBy,
 		scraperConf.MaxResultPerCall, scraperConf.ApiKey, time.Now().Add(-time.Hour), db.GetHandler(getDbClient(ctx, pgdbConf)))
+	go youtubeService.InitialiseScraper(ctx)
 
-	youtubeService.InitialiseScraper(ctx)
+	log.Println("start http server")
+	initFetchHttpServer(ctx, pgdbConf)
+}
+
+func initFetchHttpServer(ctx context.Context, config *db.Config) {
+
+	httpService := fetch.HttpService{FetchService: fetch.GetFetchService(ctx, db.GetHandler(getDbClient(ctx, config)))}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/videos/search", httpService.VideoSearch)
+	mux.HandleFunc("/videos/getpage", httpService.VideoPage)
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
 
 // initDb Create entities in PGDB
@@ -50,8 +67,7 @@ func initDb(ctx context.Context, config *db.Config) error {
 		return err
 	}
 	// create indexes
-	createIndexRes := dbClient.Exec(`CREATE INDEX IF NOT EXISTS  text_search_idx ON Videos USING GIN (to_tsvector('english', title || ' ' || description)); CREATE INDEX IF NOT EXISTS id_created_at_idx ON Videos (id, created_at);
-	CREATE INDEX IF NOT EXISTS id_created_at_idx ON Videos (created_at, id);`)
+	createIndexRes := dbClient.Exec(`CREATE INDEX IF NOT EXISTS  text_search_idx ON Videos USING GIN (to_tsvector('english', title || ' ' || description)); CREATE INDEX IF NOT EXISTS id_created_at_idx ON Videos (created_at, id);`)
 	if createIndexRes != nil {
 		return createIndexRes.Error
 	}
